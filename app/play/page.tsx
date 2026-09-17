6e9df159e1183c7fdb1f6d860cf7972736306689
keerts <keerthi.undurthi@gmail.com>
2026-09-17 11:03:06 +0000

    working version of the game

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import { useGameStore } from "@/game/store";
+import Game from "@/components/game";
 
 export default function Play() {
-    const currentRoom = useGameStore((s) => s.currentRoom);
-    const log = useGameStore((s) => s.log);
-    const move = useGameStore((s) => s.move);
-    const newGame = useGameStore((s) => s.newGame);
-
-    return (
-        <div className="p-24 font-monospace">
-            <button onClick={() => newGame()}>New Game</button>
-            <p>Current room: {currentRoom}</p>
-            <button onClick={() => move({ to: "study", label: "Go to the Study", noise: "low"})}>
-                Go to the Study
-            </button>
-            <pre>{log.map((l) => l.text).join("\n\n")}</pre>
-        </div>
-    )
+  return <Game />;
 }
